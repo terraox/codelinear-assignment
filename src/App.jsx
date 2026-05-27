69c1a1b96639e52953800a33e953270010050ccc
AadityaBasisth <basisthaaditya@gmail.com>
2026-05-27 17:01:57 +0530

    section-2 completed

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Header from './components/Header';
 import Hero from './components/Hero';
 import Solutions from './components/Solutions';
 import CloudBanking from './components/CloudBanking';
+import EfficientBanking from './components/EfficientBanking';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Hero />
         <Solutions />
         <CloudBanking />
+        <EfficientBanking />
       </main>
     </div>
   );
